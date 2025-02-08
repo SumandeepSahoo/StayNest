e8ac206cc5b3387d26ff8e2dfc183fc007d3dd41
SumandeepSahoo <sumandeepsahoo833@gmail.com>
2025-02-08 13:14:56 +0530

    Fix: Pass currUser to EJS templates

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV != "production") {
+if (process.env.NODE_ENV != "production") {
     require("dotenv").config();
 }
 const express = require("express");
@@ -9,7 +9,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./Utils/ExpressError.js");
 const session = require("express-session");
-const MongoStore = require('connect-mongo');
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -21,11 +21,9 @@ const userRouter = require("./routes/user.js");
 
 const dbUrl = process.env.ATLASDB_URL;
 
-main().then(() => {
-    console.log("connected to server");
-}).catch((err) => {
-    console.log(err);
-});
+main()
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log(err));
 
 async function main() {
     await mongoose.connect(dbUrl);
@@ -33,18 +31,18 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-const store = MongoStore.create ({
+const store = MongoStore.create({
     mongoUrl: dbUrl,
     crypto: {
         secret: process.env.SECRET,
     },
-    touchAfter: 24*3600,
+    touchAfter: 24 * 3600,
 });
 
 const sessionOption = {
@@ -59,30 +57,24 @@ const sessionOption = {
     },
 };
 
-// app.get("/", (req, res) => {
-//     res.send("Hi, I am root");
-// });
-
-app.get("/", (req, res) => {
-    res.render("listings/home");
-});
-
-
 app.use(session(sessionOption));
 app.use(flash());
-
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
 
+passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
+    res.locals.currUser = req.user || null;
     next();
+});
+
+app.get("/", (req, res) => {
+    res.render("listings/home");
 });
 
 app.use("/listings", listingRouter);
@@ -94,11 +86,11 @@ app.all("*", (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    let {statusCode=500, message="Something went wrong!"} = err;
-    // res.status(statusCode).send(message);
-    res.status(statusCode).render("listings/error.ejs", {message});
+    let { statusCode = 500, message = "Something went wrong!" } = err;
+    res.status(statusCode).render("listings/error.ejs", { message });
 });
 
+
 app.listen(8080, () => {
-    console.log("server is listening to port 8080");
+    console.log("Server is listening on port 8080");
 });
